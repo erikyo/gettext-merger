42572e799376929a6fade9ce003f790697ce3593
Erik Golinelli <erik@codekraft.it>
2024-03-22 23:13:22 +0100

    related tests fix

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,9 +1,13 @@
 // Import the required modules
-import { describe, expect, it } from 'vitest'
-import { gettextMerger } from '../src/cli'
+import { describe, expect, it, vi } from 'vitest'
+import * as mergePotFile from '../src/cli'
 
 describe('gettextMerger', () => {
 	it('merges input files and writes output file', async () => {
+		require = {
+			main: null,
+		} as any
+
 		process.argv = [
 			'',
 			'',
@@ -14,21 +18,6 @@ describe('gettextMerger', () => {
 			'output.po',
 		]
 
-		await gettextMerger()
-	})
-
-	it('Throws an error when the number of input files is less than 2', async () => {
-		// Mock the mergePotFile function to simulate error
-		// Mock the module using jest.unstable_mockModule
-
-		// Catch the error thrown by gettextMerger
-		try {
-			await gettextMerger()
-		} catch (error) {
-			// Ensure the error message matches the expected error message
-			expect((error as Error).message).toBe(
-				'You must provide at least two input files.'
-			)
-		}
+		expect(await mergePotFile.gettextMerger()).toBe(undefined)
 	})
 })
--- tests/mergePotObjects.test.ts
+++ tests/mergePotObjects.test.ts
@@ -6,16 +6,16 @@ describe('mergePotFiles', () => {
 		const block1: Block = new Block([])
 		block1.msgid = 'asd'
 		block1.comments = {
-			extracted: ['#. Translator comment'],
-			reference: ['#: app.js'],
+			extracted: ['Translator comment'],
+			reference: ['app.js'],
 		}
 		const blockGroup1 = new SetOfBlocks([block1])
 
 		const block2: Block = new Block({
 			msgid: 'asd',
 			comments: {
-				extracted: ['#. Translator comment'],
-				reference: ['#: app.js'],
+				extracted: ['Translator comment'],
+				reference: ['app.js'],
 			},
 		} as Block)
 		const blockGroup2 = new SetOfBlocks([block2])
@@ -23,8 +23,8 @@ describe('mergePotFiles', () => {
 		const blocke: Block = new Block([])
 		blocke.msgid = 'asd'
 		blocke.comments = {
-			extracted: ['#. Translator comment'],
-			reference: ['#: app.js'],
+			extracted: ['Translator comment'],
+			reference: ['app.js'],
 		}
 		const result = mergePotObject([blockGroup1, blockGroup2]).blocks
 		expect(result[0].toStr()).toEqual(blocke.toStr())
